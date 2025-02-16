1cb13e50d4fa75291a63db6022bc4adbca7ac4fd
LAPTOP-N68OED29\yanuv <yanuv.stein@gmail.com>
2025-02-16 16:29:39 +0200

    Updated R/zzz.R script

--- R/zzz.R
+++ R/zzz.R
@@ -6,21 +6,44 @@
   required_packages <- c("scCustomize", "stringdist", "cowplot", "ggplot2",
                          "reshape2", "scales", "plotly", "lubridate", "Seurat", "svMisc")
 
-  # Check if each package is installed, and install missing ones
-  for (pkg in required_packages) {
-    if (!requireNamespace(pkg, quietly = TRUE)) {
-      message(paste("Installing missing package:", pkg))
-      install.packages(pkg)  # Install the missing package from CRAN
-    } else {
-      message(paste(pkg, "is already installed. Skipping installation."))
+  # Ask user if they want to install dependencies
+  install_dependencies <- readline("Do you want to install missing dependencies? (yes/no): ")
+
+  # Function to install missing packages
+  install_missing_packages <- function(packages) {
+    for (pkg in packages) {
+      if (!requireNamespace(pkg, quietly = TRUE)) {
+        message(paste("Installing missing package:", pkg))
+        tryCatch({
+          if (pkg == "scCustomize" && !requireNamespace(pkg, quietly = TRUE)) {
+            remotes::install_github("samuel-marsh/scCustomize")
+          } else {
+            install.packages(pkg)
+          }
+        }, error = function(e) {
+          message(paste("Failed to install package:", pkg))
+        })
+      } else {
+        message(paste(pkg, "is already installed."))
+      }
     }
   }
 
-  # If any of the packages are not available from CRAN (e.g., GitHub versions),
-  # you can add custom installation commands for them here.
-  if (!requireNamespace("scCustomize", quietly = TRUE)) {
-    message("Installing scCustomize from GitHub")
-    remotes::install_github("samuel-marsh/scCustomize")
+  # Install dependencies if user agrees
+  if (tolower(install_dependencies) == "yes") {
+    missing_packages <- required_packages[sapply(required_packages, function(pkg) !requireNamespace(pkg, quietly = TRUE))]
+    if (length(missing_packages) > 0) {
+      install_missing_packages(missing_packages)
+    } else {
+      message("All dependencies are already installed.")
+    }
   }
 
+  # Now install Umethod package if not already installed
+  if (!requireNamespace("Umethod", quietly = TRUE)) {
+    message("Installing Umethod package...")
+    devtools::install_github("YanuvS/Umethod")
+  } else {
+    message("Umethod package is already installed.")
+  }
 }
