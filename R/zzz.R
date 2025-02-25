88404353fa66bb10c74a4773fcf8987631ccf73c
LAPTOP-N68OED29\yanuv <yanuv.stein@gmail.com>
2025-02-25 20:23:58 +0200

    updated zzz.R file

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,10 @@
 .onLoad <- function(libname, pkgname) {
-
   required_packages <- c("stringdist", "cowplot", "ggplot2", "reshape2",
-                         "scales", "plotly", "lubridate", "Seurat", "svMisc","Matrix")
+                         "scales", "plotly", "lubridate", "Seurat", "svMisc", "Matrix")
 
-  # Install missing packages without prompting (using utils::install.packages)
-  for (pkg in required_packages) {
-    if (!requireNamespace(pkg, quietly = TRUE)) {
-      packageStartupMessage(paste("Installing missing package:", pkg))
-      utils::install.packages(pkg) # Use utils::install.packages
-    }
+  missing_packages <- required_packages[!sapply(required_packages, requireNamespace, quietly = TRUE)]
+
+  if (length(missing_packages) > 0) {
+    packageStartupMessage("Please install the following missing packages: ", paste(missing_packages, collapse = ", "))
   }
 }
